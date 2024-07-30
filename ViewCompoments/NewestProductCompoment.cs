713f5715ffc8378e445052c493fd3a183ea6d433
PBGaming11 <phanbaobeo789@gmail.com>
2024-07-30 08:30:31 +0700

    uipdate

---
+++ ViewCompoments/NewestProductCompoment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Website.Data;
+
+namespace Website.ViewCompoments
+{
+    public class NewestProductCompoment :ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NewestProductCompoment(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var sanpham = _db.sanpham.ToList();
+            return View(sanpham);
+        }
+    }
+}
